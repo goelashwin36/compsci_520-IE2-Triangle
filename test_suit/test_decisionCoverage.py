779139303a6030c1b433febbbe8e805942db0086
Ashwin Goel <goelashwin36@gmail.com>
2025-03-22 01:46:33 -0400

    Added decision coverage

--- test_suit/test_decisionCoverage.py
+++ test_suit/test_decisionCoverage.py
@@ -17,7 +17,17 @@ class TriangleTest(unittest.TestCase):
         self.assertEqual(actual, expected)
 
     def test_is_invalid2(self):
-        actual = Triangle.classify(-2, 0, 5)
+        actual = Triangle.classify(-2, 5, 5)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+    def test_is_invalid3(self):
+        actual = Triangle.classify(0, 5, 5)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+    def test_is_invalid4(self):
+        actual = Triangle.classify(1, 1, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
@@ -37,9 +47,30 @@ class TriangleTest(unittest.TestCase):
         self.assertEqual(actual, expected)
 
     def test_is_scalene1(self):
-        actual = Triangle.classify(2, 4, 5)
+        actual = Triangle.classify(2, 3, 4)
         expected = Triangle.Type.SCALENE
         self.assertEqual(actual, expected)
+
+    def test_is_scalene2(self):
+        actual = Triangle.classify(7, 8, 9)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
+
+    def test_is_edge_case1(self):
+        actual = Triangle.classify(2, 2, 4)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+    def test_is_edge_case2(self):
+        actual = Triangle.classify(4, 2, 2)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+    def test_is_edge_case3(self):
+        actual = Triangle.classify(2, 4, 2)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
